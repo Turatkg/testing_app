95ba21c7b60209ad0eeca06ffc76e216292fb144
Turat Alybaev <66704600+Turatkg@users.noreply.github.com>
2021-05-15 16:56:52 +0600

    Бир жолу иштелип бүттү, бирок азыр суроолор менен жооптор бири-бирине байланыштуу эмес

---
+++ lib/ui/consts.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+const Icon tuuraIcon = Icon(Icons.mood, color: Colors.green);
+const Icon kataIcon = Icon(Icons.mood_bad, color: Colors.red);
+
+List<String> suroolor = [
+  '1. Титаник дүйнөдөгү эң чоң кемеби? false',
+  '2. Дүйнөдөгү тооктордун саны адам санынан көп, true',
+  '3. Көпөлөктөрдүн бардык бир эле күн жашайт? false',
+  '4. Жер шары түз false,',
+  '5. Грек жаңгагынын мекени Арстанбап, true',
+  '6. Фатих Султан Мехмет өмүрүндө картошка жеген эмес true',
+  '7. Баткендин борбору шаары Кызыл-Кыя false'
+];
+
+List<bool> jooptor = [
+  false,
+  true,
+  false,
+  false,
+  true,
+  true,
+  false,
+];
