cec3298dd2d0f16228bf96e9ae0b7e044382b56c
Turat Alybaev <66704600+Turatkg@users.noreply.github.com>
2021-05-17 23:56:49 +0600

    Суроо топтому бул жактан чыгарылды

--- lib/data/quastions.dart
+++ lib/data/quastions.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Suroo {
   String surooTexti;
@@ -6,19 +6,3 @@ class Suroo {
 
   Suroo({@required this.surooTexti, @required this.jooptoru});
 }
-
-List<Suroo> surooToptomu = [
-  Suroo(surooTexti: '1. Титаник дүйнөдөгү эң чоң кемеби?', jooptoru: false),
-  Suroo(
-      surooTexti: '2. Дүйнөдөгү тооктордун саны адам санынан көп',
-      jooptoru: true),
-  Suroo(
-      surooTexti: '3. Көпөлөктөрдүн бардык түрү бир эле күн жашайт?',
-      jooptoru: false),
-  Suroo(surooTexti: '4. Жер шары түз', jooptoru: false),
-  Suroo(surooTexti: '5. Грек жаңгагынын мекени Арстанбап', jooptoru: true),
-  Suroo(
-      surooTexti: '6. Фатих Султан Мехмет өмүрүндө картошка жеген эмес',
-      jooptoru: true),
-  Suroo(surooTexti: '7. Баткендин борбору шаары Кызыл-Кыя', jooptoru: false),
-];
